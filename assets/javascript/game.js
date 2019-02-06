40f235a02c810a863c88fcc3cade9a9910970d28
Eleonore Chevillion <el.chevillion@gmail.com>
2019-02-05 21:07:00 -0500

    beginning of game

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+
+var WordBank = ["DOG", "CAT", "TURTLE"];
+var randomWord = WordBank[Math.floor(Math.random() * WordBank.length)];
+var ChosenWord = randomWord[""].split("");
+console.log(ChosenWord);
